301557930ab6975ea33b47b8e7a443c9cd5e548b
BajiLinux <mr.baji.smart@gmail.com>
2025-12-11 19:46:36 +0700

    First Commit to D6

---
+++ D6_ComponentsAndProps/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <>
+    <UserProfile></UserProfile>
+    <Button/>
+    </>
+  );
+}
+
+export default App;
