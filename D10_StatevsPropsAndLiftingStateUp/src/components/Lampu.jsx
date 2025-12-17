37d8b8018644afab06d985f9bbc0318f6dca7cc3
BajiLinux <mr.baji.smart@gmail.com>
2025-12-17 21:49:38 +0700

    Done belajar props, props itu kepanjangan dari properties atau attribute dan contoh <Button click={clickMe} /> yang di sebelah kiri (click) itu adalah props dan di sebelah kanan (clickMe) adalah nilai props atau mirip-mirip sama key dan value dan gunanya props itu buat ngirim data dari parent ke child gitu aja sih singkatnya

--- D10_StatevsPropsAndLiftingStateUp/src/components/Lampu.jsx
+++ D10_StatevsPropsAndLiftingStateUp/src/components/Lampu.jsx
@@ -1,6 +1,12 @@
 function Lampu({isOn}) {
+  const lampStyle = `w-20 h-20 rounded-full flex items-center justify-center font-bold text-white transition-all duration-300 ${
+    isOn ? "bg-yellow-400 shadow-[0_0_20px_rgba(250,204,21,0.7)]" : "bg-gray-700"
+  }`;
   return (
-    <div>Lampu: {isOn ? "Menyala" : "Mati"}</div>
+    <div className="flex flex-col items-center gap-5 mb-5">
+    <div className={`${lampStyle}`}></div>
+      <p>Ruang Tamu</p>
+    </div>
   );
 };
 
--- D10_StatevsPropsAndLiftingStateUp/src/components/Saklar.jsx
+++ D10_StatevsPropsAndLiftingStateUp/src/components/Saklar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 function Saklar({isOn, togglerSwitch}) {
   return (
-    <button className={`bg-amber-400 px-5 py-3 hover:bg-amber-600 pointer-events-auto`} onClick={togglerSwitch}>Turn {isOn ? "Off" : "On"}</button>
-  )
-}
+    <div className="flex flex-col items-center mb-5">
+      <button className={`bg-amber-400 px-5 py-3 hover:bg-amber-600 active:bg-amber-800 cursor-pointer`} onClick={togglerSwitch}>Turn {isOn ? "Off" : "On"}</button>
+    </div>
+  );
+};
 
-export default Saklar
+export default Saklar;
