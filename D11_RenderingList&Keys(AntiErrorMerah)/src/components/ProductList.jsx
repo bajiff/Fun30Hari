d505b90215007e4de8c9827fe71bf9011ba1e5cc
BajiLinux <mr.baji.smart@gmail.com>
2025-12-18 20:36:35 +0700

    D11 Done lanjut ke D12 :D

--- D11_RenderingList&Keys(AntiErrorMerah)/src/components/ProductList.jsx
+++ D11_RenderingList&Keys(AntiErrorMerah)/src/components/ProductList.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
 import {products} from "../../data";
 const ProductList = () => {
-  const [localProducts, setProduct] = useState(products);
+  const [localProducts, setLocalProducts] = useState(products);
   const deleteProduct = (idProduct) => {
-    setProduct(localProducts.filter(product => product.id !== idProduct))
-  }
+    setLocalProducts(localProducts.filter(product => product.id !== idProduct));
+  };
   return (
-    <table className="table-fixed bg-green-500 border-2">
-       <thead>
+    <table>
+      <thead>
         <tr>
           <th>No</th>
           <th>Name</th>
           <th>Price</th>
           <th>Stock</th>
           <th>Category</th>
+          <th>Actions</th>
         </tr>
-       </thead>
-       <tbody>
-        {products.map(product => {
-        return(
-          <tr key={product.id}>
-            <td>{product.id}</td>
-            <td>{product.name}</td>
-            <td>{product.price}</td>
-            <td>{product.stock}</td>
-            <td>{product.category}</td>
-            <td><button className="cursor-pointer bg-red-400 rounded-2xl" onClick={() => deleteProduct(product.id)}>Delete</button></td>
-          </tr>
-        )})}
-       </tbody>
+      </thead>
+      <tbody>
+    {localProducts.map(product => (
+        <tr key={product.id}>
+          <td>{product.id}</td>
+          <td>{product.name}</td>
+          <td>{product.price}</td>
+          <td>{product.stock}</td>
+          <td>{product.category}</td>
+          <td><button onClick={() => deleteProduct(product.id)}>Delete</button></td>
+        </tr>
+      )
+    )}
+      </tbody>
     </table>
   )
 }
 
-export default ProductList;
+export default ProductList
