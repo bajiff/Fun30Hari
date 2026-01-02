24c22d8df2e00798534a3ab8248c14a2897ef637
BajiLinux <mr.baji.smart@gmail.com>
2026-01-02 18:54:52 +0700

    Jadi tanpa [] itu ibarat useEffect berjalan setiap ada perubahan di component, perubahan apapun itu, kalau dengan [] maka useEffect akan dijalankan sekali doang saat pertamakali component itu dirender, kalau ada dependencies atau [dependencies] dalam kasus ini menggunakan dependencies [pesan] jadi setiap  ada perubahan di state pesan maka jalankan useEffect nya gitu sih kuranglebihnya

--- D15-TheLifeCycleLab/src/components/ChatRoom.jsx
+++ D15-TheLifeCycleLab/src/components/ChatRoom.jsx
@@ -4,27 +4,37 @@ const ChatRoom = () => {
 
   const [pesan,setPesan] = useState("");
   const [count, setCount] = useState(0);
-  const [car,setCar] = useState("");
+  const [car] = useState("");
 
   useEffect(() => {
-    console.log("📡 Connected to Server")
-    console.log(document.title = pesan);
-    return () => {
-      console.log("❌ Disconnected from Server");
-    }
+      document.title = pesan;
   },[pesan]);
+  
+  useEffect(() => {
+    console.log("Connected");
+    return () => {
+      console.log("❌ Disconnected")
+    }
+  },[]);
+  
   
   
   const inputStyle = `mt-10 border p-1 rounded`
 
+
   return (
     <>
     <h1 className="mt-10">{car}</h1>
-    <input className={inputStyle} type="text" name="message" id="message" placeholder="Type something" required autoFocus onChange={e => setPesan(e.target.value)} />
+    <input className={inputStyle} type="text" name="message" id="message" placeholder="Type something" required autoFocus value={pesan} onChange={e => setPesan(e.target.value)} />
+
+      <button onClick={() => setPesan("")}>Reset Input</button>
+
       <p>My Girlfriends {count}</p>
       <button onClick={() => setCount(prev => prev + 1)}>Tambah</button>
+
       <button onClick={() => setCount(prev => prev - 1)}>Kurang</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+
+      <button onClick={() => setCount(0)}>Reset Number</button>
     </>
   )
 }
