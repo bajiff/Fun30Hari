74dad4777b472887f50865d140930f1db82c9953
BajiLinux <mr.baji.smart@gmail.com>
2026-01-06 23:17:24 +0700

    Baru bikin animasi loading, belum di styling make tailwindcss

--- D18-UXImporvement/src/components/UserList.jsx
+++ D18-UXImporvement/src/components/UserList.jsx
@@ -25,9 +25,54 @@ const UserList = () => {
     getAPI();
   },[]);
   
-  if (error) return <p>Waduh Error cuy: {error}</p>
-  if (loading) return <p>Sabar Lagi Loading...</p>
-
+  if (error) {
+    return (
+    <section className="flex flex-col gap-2 items-center justify-center h-screen">
+      <p>Waduh Error cuy: {error}</p>
+      <a className="px-3 py-2 rounded bg-green-600" href="/">Muat Ulang Hehew</a>
+    </section>
+    )
+  } else if (loading) {
+    return (
+    <section className="">
+      <section className="animate-pulse bg-gray-500 w-full h-3 rounded-2xl"></section>
+    </section>
+    // <section className="flex justify-center items-center min-h-screen">
+    //   <table className="table-auto border-collapse">
+    //     <thead className="border">
+    //       <tr className="text-center border bg-yellow-500">
+    //         <th className="px-4 py-2 border" rowSpan={2}>No</th>
+    //         <th className="px-4 py-2 border" rowSpan={2}>Name</th>
+    //         <th className="px-4 py-2 border" rowSpan={2}>Username</th>
+    //         <th className="px-4 py-2 border" rowSpan={2}>Email</th>
+    //         <th className="px-4 py-2 border" colSpan={4}>Address</th>
+    //         <th className="px-4 py-2 border" rowSpan={2}>Phone</th>
+    //         <th className="px-4 py-2 border" rowSpan={2}>Website</th>
+    //       </tr>
+    //       <tr className="bg-red-500">
+    //         <th className="border px-4 py-2" rowSpan={2}>Street</th>
+    //         <th className="border px-4 py-2" rowSpan={2}>Suite</th>
+    //         <th className="border px-4 py-2" rowSpan={2}>City</th>
+    //         <th className="border px-4 py-2" rowSpan={2}>Zipcode</th>
+    //       </tr>
+    //     </thead>
+    //       <tbody>
+    //           <tr className="text-center">
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //             <td className="px-4 py-3 border"></td>
+    //           </tr>
+    //       </tbody>
+    //   </table>
+    // </section>
+  )} else {
   return (
     <section className="flex justify-center items-center min-h-screen">
       <table className="table-auto border-collapse">
@@ -66,7 +111,8 @@ const UserList = () => {
           </tbody>
       </table>
     </section>
-  )
+    )
+  }
 }
 
 export default UserList;
