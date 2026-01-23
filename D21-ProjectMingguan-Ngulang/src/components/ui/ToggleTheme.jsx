9520ca6408b51e5fabcecce370966d3bcff3baad
BajiLinux <mr.baji.smart@gmail.com>
2026-01-23 18:03:31 +0700

    Done

--- D21-ProjectMingguan-Ngulang/src/components/ui/ToggleTheme.jsx
+++ D21-ProjectMingguan-Ngulang/src/components/ui/ToggleTheme.jsx
@@ -4,7 +4,7 @@ import { ThemeContext } from "../../context";
 const ToggleTheme = () => {
   const {theme,setTheme} = useContext(ThemeContext);
   return (
-    <button className={`rounded shadow-2xl shadow-gray-500 px-6 py-2 transition-all active:scale-95 ${theme === "light" ? "bg-slate-800 text-white" : "bg-yellow-600 text-slate-400"}`} onClick={() => setTheme(theme === "light" ? "dark" : "light")} title={`${theme === "light" ? "Ubah ke Dark" : "Ubah ke Light"}`}>{theme === "light" ? "🌞" : "🌙"}</button>
+      <button className={`duration-300 rounded shadow-2xl shadow-gray-500 px-6 py-2 transition-all hover:rotate-90 active:scale-95 ${theme === "light" ? "bg-slate-800 text-white" : "bg-yellow-600 text-slate-400"}`} onClick={() => setTheme(theme === "light" ? "dark" : "light")} title={`${theme === "light" ? "Ubah ke Dark" : "Ubah ke Light"}`}>{theme === "light" ? "🌞" : "🌙"}</button>
   )
 }
 
