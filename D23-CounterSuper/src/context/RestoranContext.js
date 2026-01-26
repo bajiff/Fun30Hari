9d44c026cf4b3bd0bd391553b999240cf002507b
BajiLinux <mr.baji.smart@gmail.com>
2026-01-26 15:00:43 +0700

    Mulai ngoding lagi, awal mula bikin useReducer yaitu kita bikin contextnya dulu nih di src/context seharusnya contexts ada 's'nya tapi udah terlanjur jadi yaudahlah, lalu kita bikin RestonranContext.js yang berisi inisialisasidata, logika bisnis, createcontextnya atau bikin wadahnya, lalu setelah bikin wadah kita bikin pembungkusnya atau Providernya

--- D23-CounterSuper/src/context/RestoranContext.js
+++ D23-CounterSuper/src/context/RestoranContext.js
@@ -1,5 +1,27 @@
 import { createContext } from "react";
 
-const RestoranContext = createContext();
+// ? Inisialisasi data
+const initialState = {
+  stokTelur:0,
+  statusToko:"BUKA"
+};
 
-export default RestoranContext;
+// ? Bikin Logika Bisnis
+const restoranReducer = (state,action) {
+  switch (action.type) {
+    case "BELI_TELUR":
+      return {...state, stokTelur : state.stokTelur + 1};
+    case "MASAK_TELUR":
+      return {...state, stokTelur : state.stokTelur - 1};
+    case "TUTUP_TOKO":
+      return {...state, statusToko: state.statusToko = "TUTUP"};
+    default:
+      return state;
+  };
+};
+
+// ? Create Contextnya atau wadahnya
+export const RestoranContext = createContext();
+
+
+
