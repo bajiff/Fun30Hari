1a5ea4c24a01a80d60e52d70c6d9dde8fc2fb675
BajiLinux <mr.baji.smart@gmail.com>
2026-01-28 13:35:53 +0700

    Lanjut belajar useReducer

--- D23-CounterSuper/src/App.jsx
+++ D23-CounterSuper/src/App.jsx
@@ -1,6 +1,5 @@
 // ? Ini App.jsx
 import {Card, ToggleTheme} from "./components/ui"; 
-import BankPintar from "./components/ui/BankPintar";
 import DompetBos from "./components/ui/DompetBos";
 
 import { ThemeContext } from "./context";
@@ -16,7 +15,6 @@ const App = () => {
         <Card/>
         <ToggleTheme/>
         <DompetBos/>
-        <BankPintar/>
       </section>
     </ThemeContext.Provider>
   );
